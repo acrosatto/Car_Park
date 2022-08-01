bf043e4407317d0d2ec0a94f3eb3c354c2e32edc
Ana Crosatto <99294383+acrosatto@users.noreply.github.com>
2022-08-01 11:44:15 -0300

    Car Park Files
    Adding files into the repository

---
+++ functions.py
@@ -0,0 +1,431 @@
+import psycopg2
+from datetime import datetime
+from time import sleep
+
+
+class Start:
+    """Start the program. Get title and frase to use duting the prgram"""
+
+    def __init__(self, frase):
+        self.frase = frase
+
+    def get_title(self):
+        return f'\033[37m{"==" * 30}\033[m\n{self.frase.center(60)}\n\033[37m{"==" * 30}\033[m'
+
+    def get_frase(self):
+        return f'\033[1;32m{self.frase}\033[m'
+
+
+class DB:
+    """Connect the functions to PostgreSQL to insert, update or delete data from the tables clients and parking"""
+    connection = None
+
+    def __init__(self):
+        self.host = 'localhost'
+        self.dbname = 'carpark'
+        self.username = 'postgres'
+        self.password = 'lovepython!'
+        self.port = 5432
+
+    def connect_psql(self):
+        try:
+            connection = psycopg2.connect(host=self.host, dbname=self.dbname, user=self.username,
+                                          password=self.password, port=self.port)
+            return connection
+        except (Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL", error)
+
+
+class Client:
+    """Input new clients, retrieve or update info on already registered clients and get list of all clients"""
+
+    @staticmethod
+    def new_client_in():
+        print()
+        s = Start("NEW CLIENT")
+        print(s.get_title())
+        sleep(1)
+        try:
+            # Connecting to postgres
+            connection = DB()
+            connection = connection.connect_psql()
+            cursor = connection.cursor()
+
+            cursor.execute("SELECT * FROM parking WHERE status = 'IN';")
+            # Insert new client's info into the table clients. That will trigger the table parking to be filled
+            # with: date_in, time_in and status='IN'.
+            person = [(input("First Name: ").title(),
+                       input("Last Name: ").title(),
+                       input("Make: ").title(),
+                       input("Color: "),
+                       input("License Plate: "))]
+            for p in person:
+                cursor.execute("INSERT INTO clients("
+                               "first_name, last_name, make, color, license_plate)"
+                               "VALUES(%s, %s, %s, %s, %s);", p)
+            print(f"\033[32m'Ready. New Client inserted successfully.'\033[m")
+            print()
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+        except (Exception, psycopg2.Error):
+            print("\033[31mError, please try again.", "\033[m")
+
+    @staticmethod
+    def regular_client_in():
+        print()
+        s = Start("CLIENT")
+        print(s.get_title())
+        sleep(1)
+        try:
+            connection = DB()
+            connection = connection.connect_psql()
+            cursor = connection.cursor()
+
+            # cursor.execute("SELECT * FROM parking WHERE status = 'IN';")
+            # generating date and time
+            now = datetime.now()
+            date_in = now.date()
+            time_in = now.strftime("%Y/%m/%d, %H:%M:%S")
+            status = 'IN'
+
+            # Regular client entering the car park.
+            person = [(input("License Plate: "),
+                       date_in,
+                       time_in,
+                       status)]
+            for p in person:
+                cursor.execute("INSERT INTO parking(license_plate, date_in, time_in, status) "
+                               "VALUES(%s, %s, %s , %s);", p)
+                print(f"\033[32m'Ready. Client IN.'\033[m")
+            print()
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+        except (Exception, psycopg2.Error):
+            print("\033[31mError, please try again.", "\033[m")
+
+    @staticmethod
+    def get_client_info():
+        print()
+        s = Start("CLIENT'S INFO")
+        print(s.get_title())
+        sleep(1)
+        try:
+            connection = DB()
+            connection = connection.connect_psql()
+            cursor = connection.cursor()
+
+            customer = [(input("License Plate: "))]
+            cursor.execute("SELECT first_name, last_name, make, color"
+                           " FROM clients WHERE license_plate = %s;"
+                           , customer)
+
+            # Info of a client, from the table clients.
+            results = cursor.fetchmany(4)
+            print("Customer's Details:")
+            for result in results:
+                print("\033[37mFirst Name:\033[m", result[0])
+                print("\033[37mLast Name:\033[m", result[1])
+                print("\033[37mMake:\033[m", result[2])
+                print("\033[37mColor:\033[m", result[3], "\n")
+            connection.commit()
+
+            cursor.execute("SELECT time_in, time_out, price, status FROM parking WHERE license_plate = %s"
+                           "ORDER BY date_in DESC;"
+                           , customer)
+            results = cursor.fetchmany(4)
+            print("Car Park entries:")
+            for result in results:
+                print("\033[37m\t-Entry:\033[m", result[0])
+                if result[3] == 'IN':
+                    print("\033[1;37m\t-Status:\033[1;32m", result[3], "\033[m", "\n")
+                else:
+                    print("\033[37m\t-Out:\033[m", result[1])
+                    print("\033[37m\t-Paid:\033[m$", result[2])
+                    print("\033[1;37m\t-Status:\033[1;31m", result[3], "\033[m", "\n")
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+        except (Exception, psycopg2.Error):
+            print("\033[31mError, please try again.", "\033[m")
+
+    @staticmethod
+    def clients_list():
+        print()
+        s = Start("LIST OF CLIENTS")
+        print(s.get_title())
+        sleep(1)
+        try:
+            connection = DB()
+            connection = connection.connect_psql()
+            cursor = connection.cursor()
+
+            # List of all the car park's clients.
+            cursor.execute("SELECT * FROM clients ORDER BY customer_number;")
+            results = cursor.fetchall()
+            for result in results:
+                print("\033[37mCustomer Number:\033[m", result[0])
+                print("\033[37mFirst Name:\033[m", result[1])
+                print("\033[37mLast Name:\033[m", result[2])
+                print("\033[37mMake:\033[m", result[3])
+                print("\033[37mColor:\033[m", result[4])
+                print("\033[37mLicense Plate:\033[m", result[5])
+                print("--" * 30)
+            connection.commit()
+            cursor.close()
+            connection.close()
+        except (Exception, psycopg2.Error):
+            print("\033[31mError, please try again.", "\033[m")
+
+    @staticmethod
+    def update_car_details():
+        print()
+        s = Start("UPDATE INFO")
+        print(s.get_title())
+        sleep(1)
+        try:
+            connection = DB()
+            connection = connection.connect_psql()
+            cursor = connection.cursor()
+
+            # Update customer's car details
+            old_license = (input("Old License Plate: "))
+            new_license = (input("New License Plate: "))
+            make = (input("Make: "))
+            color = (input("Color: "))
+
+            cursor.execute("UPDATE clients SET license_plate = %s, make = %s, color = %s WHERE license_plate = %s;",
+                           (new_license, make, color, old_license))
+
+            print("\033[37mUpdated successfully.\033[m")
+            connection.commit()
+            cursor.close()
+            connection.close()
+        except (Exception, psycopg2.Error):
+            print("\033[31mError, please try again.", "\033[m")
+
+    @staticmethod
+    def update_customer_details():
+        print()
+        s = Start("UPDATE INFO")
+        print(s.get_title())
+        sleep(1)
+        try:
+            connection = DB()
+            connection = connection.connect_psql()
+            cursor = connection.cursor()
+
+            # Update customer's personal details
+            license_plate = (input("License Plate: "))
+            first_name = (input("First Name: "))
+            last_name = (input("Last Name: "))
+
+            cursor.execute("UPDATE clients SET first_name = %s, last_name = %s WHERE license_plate = %s;",
+                           (first_name, last_name, license_plate))
+
+            print("\033[37mUpdated successfully.\033[m")
+            connection.commit()
+            cursor.close()
+            connection.close()
+        except (Exception, psycopg2.Error):
+            print("\033[31mError, please try again.", "\033[m")
+
+
+class Management:
+    """Get the amount of cars in the carpark, manage payments and get reports"""
+
+    @staticmethod
+    def get_occupancy():
+        try:
+            connection = DB()
+            connection = connection.connect_psql()
+            cursor = connection.cursor()
+
+            total_lots = 15
+            count_lots = 0
+            cursor.execute("SELECT * FROM parking WHERE status = 'IN';")
+            lots = cursor.fetchall()
+            for lot in lots:
+                if lot[8] == 'IN':
+                    count_lots += 1
+                    if total_lots - count_lots == 0:
+                        print("\033[31mSorry, the carpark is full.\033[m")
+            print(f"- Empty lots: \033[1;32m{total_lots - count_lots}\033[m")
+            print(f"- Occupied lots: \033[1;31m{count_lots}\033[m")
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+
+        except (Exception, psycopg2.Error):
+            print("\033[31mError, please try again.", "\033[m")
+
+    @staticmethod
+    def payment():
+        print()
+        s = Start("PAYMENT/CUSTOMER OUT")
+        print(s.get_title())
+        sleep(1)
+        try:
+            connection = DB()
+            connection = connection.connect_psql()
+            cursor = connection.cursor()
+
+            # Customer leaving (time)
+            license_plate = (input("License Plate: "))
+            status = 'IN'
+            now = datetime.now()
+            time_out = now.strftime("%Y/%m/%d, %H:%M:%S")
+
+            cursor.execute("UPDATE parking SET time_out = %s "
+                           "WHERE license_plate = %s AND status = %s;",
+                           (time_out, license_plate, status))
+
+            connection.commit()
+
+            # Customer leaving (day)
+            cursor.execute("UPDATE parking SET date_out = now() "
+                           "WHERE license_plate = %s AND status = %s;",
+                           (license_plate, status))
+            connection.commit()
+
+            # Amount of time the car stayed in the parking-lot in hours
+            cursor.execute("UPDATE parking SET time_period = ((EXTRACT(DAY FROM date_out::timestamp - "
+                           "date_in::timestamp) * (24 * 60)) + (EXTRACT(HOUR FROM time_out::time "
+                           "- time_in::time) * 60) + EXTRACT(MINUTES FROM time_out::time - time_in::time)) / 60"
+                           " WHERE license_plate=%s AND status = %s;",
+                           (license_plate, status))
+            connection.commit()
+            cursor.execute("UPDATE parking SET time_period = ROUND(time_period::numeric, 2) "
+                           "WHERE license_plate=%s AND status = %s;",
+                           (license_plate, status))
+            connection.commit()
+
+            # Amount to be paid: time stayed in the car park by the price $10 per hour
+            cursor.execute(
+                "UPDATE parking SET price = time_period * 10 "
+                "WHERE license_plate=%s AND status = %s;", (license_plate, status))
+            connection.commit()
+            cursor.execute("UPDATE parking SET price = ROUND(price::numeric, 2) "
+                           "WHERE license_plate=%s AND status = %s;",
+                           (license_plate, status))
+            connection.commit()
+
+            # Status of the customer/car IN or OUT
+            cursor.execute("UPDATE parking SET status = CASE WHEN price > 0 THEN 'OUT' ELSE 'IN' "
+                           "END WHERE license_plate = %s AND status = %s;",
+                           (license_plate, status))
+            connection.commit()
+
+            cursor.execute("SELECT time_period, price FROM parking WHERE license_plate = %s AND time_out = %s;",
+                           (license_plate, time_out))
+            record = cursor.fetchmany(2)
+            for r in record:
+                print(f'Parking Period: {r[0]} hours')
+                print(f'Total: ${r[1]}')
+                print("\033[32m'PAID'\033[m")
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+
+        except (Exception, psycopg2.Error):
+            print("\033[31mError, please try again.", "\033[m")
+
+    @staticmethod
+    def get_cars_in_now():
+        sleep(1)
+        try:
+            connection = DB()
+            connection = connection.connect_psql()
+            cursor = connection.cursor()
+
+            # Report retrieved from the db table
+            cursor.execute("SELECT c.first_name, c.last_name, c.make, c.color, c.license_plate,"
+                           " p.time_in, p.time_out, p.time_period, p.price, p.status"
+                           " FROM clients AS c"
+                           " JOIN parking AS p"
+                           " ON c.license_plate = p.license_plate"
+                           " WHERE p.status = 'IN';")
+            results = cursor.fetchall()
+            print()
+            for result in results:
+                if result[9] == 'IN':
+                    print("Client:")
+                    print("\t\033[37m-First Name:\033[m", result[0])
+                    print("\t\033[37m-Last Name:\033[m", result[1])
+                    print("Car:")
+                    print("\t\033[37m-Make:\033[m", result[2])
+                    print("\t\033[37m-Color:\033[m", result[3])
+                    print("\t\033[37m-License Plate:\033[m", result[4])
+                    print("\033[37mDate IN:\033[m", result[5])
+                    print("\033[1;37mStatus:\033[1;32m", result[9], "\033[m")
+                else:
+                    pass
+                print("--" * 30)
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+        except (Exception, psycopg2.Error):
+            print("\033[31mError, please try again.", "\033[m")
+
+    @staticmethod
+    def get_report():
+        print()
+        s = Start("CAR PARK'S REPORT")
+        print(s.get_title())
+        sleep(1)
+        try:
+            connection = DB()
+            connection = connection.connect_psql()
+            cursor = connection.cursor()
+
+            # Report retrieved from the db table
+            from_date = input("From (YYYY/MM/DD): ")
+            from_report = datetime.strptime(from_date, "%Y/%m/%d").date()
+            to_date = input("To (YYYY/MM/DD): ")
+            to_report = datetime.strptime(to_date, "%Y/%m/%d").date()
+            cursor.execute("SELECT c.first_name, c.last_name, c.make, c.color, c.license_plate,"
+                           " p.time_in, p.time_out, p.time_period, p.price, p.status"
+                           " FROM clients AS c"
+                           " JOIN parking AS p"
+                           " ON c.license_plate = p.license_plate"
+                           " WHERE p.date_in BETWEEN %s AND %s ORDER BY p.date_in;",
+                           (from_report, to_report))
+            results = cursor.fetchall()
+            print()
+            for result in results:
+                print("Client:")
+                print("\t\033[37m-First Name:\033[m", result[0])
+                print("\t\033[37m-Last Name:\033[m", result[1])
+                print("Car:")
+                print("\t\033[37m-Make:\033[m", result[2])
+                print("\t\033[37m-Color:\033[m", result[3])
+                print("\t\033[37m-License Plate:\033[m", result[4])
+                print("\033[37mDate and Time IN:\033[m", result[5])
+                if result[9] == 'IN':
+                    print("\033[1;37mStatus:\033[1;32m", result[9], "\033[m")
+                else:
+                    print("\033[37mDate and Time OUT:\033[m", result[6])
+                    print("\033[37mPeriod:\033[m", result[7], "hours")
+                    print("\033[37mPrice:\033[m $", result[8])
+                    print("\033[1;37mStatus:\033[1;31m", result[9], "\033[m")
+                print("--" * 30)
+
+            # Amount of money made in that time period
+            cursor.execute("SELECT sum(price) AS total FROM parking WHERE date_in BETWEEN %s AND %s;",
+                           (from_report, to_report))
+            total = cursor.fetchone()
+            for t in total:
+                print("\033[1;32mMoney IN: $", t, "\033[m")
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+        except (Exception, psycopg2.Error):
+            print("\033[31mError, please try again.", "\033[m")
---
+++ program.py
@@ -0,0 +1,154 @@
+from functions import *
+
+
+title = Start("""\033[1;33m
+            ╦ ╦╔═╗╦  ╔═╗╔═╗╔╦╗╔═╗  ╔╦╗╔═╗  ╔╦╗╦ ╦╔═╗  
+            ║║║║╣ ║  ║  ║ ║║║║║╣    ║ ║ ║   ║ ╠═╣║╣   
+            ╚╩╝╚═╝╩═╝╚═╝╚═╝╩ ╩╚═╝   ╩ ╚═╝   ╩ ╩ ╩╚═╝  
+                    ╔═╗╔═╗╦═╗  ╔═╗╔═╗╦═╗╦╔═           
+                    ║  ╠═╣╠╦╝  ╠═╝╠═╣╠╦╝╠╩╗           
+                    ╚═╝╩ ╩╩╚═  ╩  ╩ ╩╩╚═╩ ╩           
+ \033[m""")
+print(title.get_title())
+
+s = Start("OCCUPANCY NOW:")
+print(s.get_frase())
+Management.get_occupancy()
+
+spots = 15
+
+sleep(0.5)
+while True:
+    print()
+    s = Start("MAIN MENU")
+    print(s.get_title())
+    main_menu = Start('''\n\033[1;33mCAR PARK\033[m     
+             [ 𝟙 ]  Clients 
+             [ 𝟚 ]  Management
+             [ 𝟛 ]  END Program...''')
+    print(main_menu.get_frase())
+    try:
+        choice = int(input("\n>>> Please choose an option: "))
+        if choice == 1:
+            sleep(0.5)
+            while True:
+                print()
+                s = Start("CLIENT'S MENU")
+                print(s.get_title())
+                client_menu = Start('''\n\033[1;32mCLIENT\033[m     
+                [ 𝟙 ]  New Client IN 
+                [ 𝟚 ]  Recurring Client IN
+                [ 𝟛 ]  Client OUT(payment)
+                [ 𝟜 ]  Client details(check/update)
+                [ 𝟝 ]  Back to MAIN MENU''')
+                print(client_menu.get_frase())
+                client_choice = int(input("\n>>> Choose an option: "))
+                if spots <= 0 and client_choice == 1:
+                    print("\033[1;31mSorry, the carpark is full.\033[m")
+                elif client_choice == 1 and spots >= 1:
+                    Client.new_client_in()
+                    spots -= 1
+                    sleep(1)
+                elif client_choice == 2 and spots <= 0:
+                    print("\033[1;31mSorry, the carpark is full.\033[m")
+                elif client_choice == 2 and spots >= 0:
+                    Client.regular_client_in()
+                    spots -= 1
+                    sleep(1)
+                elif client_choice == 3:
+                    Management.payment()
+                    spots += 1
+                    sleep(1)
+                elif client_choice == 4:
+                    while True:
+                        print()
+                        s = Start("CLIENTS DETAILS")
+                        print(s.get_title())
+                        update_menu = Start('''\n\033[1;32mINFO\033[m 
+                [ 𝟙 ]  UPDATE Client's Info
+                [ 𝟚 ]  UPDATE Car's Info
+                [ 𝟛 ]  Check Client's Info
+                [ 𝟜 ]  Back to CLIENTS MENU''')
+                        print(update_menu.get_frase())
+                        update_choice = int(input("\n>>> Choose an option: "))
+                        if update_choice == 1:
+                            Client.update_customer_details()
+                        elif update_choice == 2:
+                            Client.update_car_details()
+                        elif update_choice == 3:
+                            Client.get_client_info()
+                        elif update_choice == 4:
+                            break
+                        else:
+                            print("\033[31mError, try again.\033[m")
+                            continue
+                elif client_choice == 5:
+                    break
+                else:
+                    print("\033[31mError, please try again.\033[m")
+                    print()
+                    print(" " * 15, " . " * 10, " " * 10)
+                    continue
+        elif choice == 2:
+            sleep(0.3)
+            while True:
+                print()
+                s = Start("MANAGEMENT'S MENU")
+                print(s.get_title())
+                print()
+                management_menu = Start('''MANAGEMENT\033[m
+                [ 𝟙 ]  Occupancy now
+                [ 𝟚 ]  Report per date
+                [ 𝟛 ]  Client's list
+                [ 𝟜 ]  Back to MAIN MENU''')
+                print(management_menu.get_frase())
+                management_choice = int(input("\n>>> Choose an option: "))
+                if management_choice == 1:
+                    sleep(1)
+                    print()
+                    s = Start("OCCUPANCY")
+                    print(s.get_title())
+                    Management.get_occupancy()
+                    print()
+                    s = Start("CAR'S IN NOW")
+                    print(s.get_frase())
+                    Management.get_cars_in_now()
+                    sleep(1)
+                elif management_choice == 2:
+                    sleep(1)
+                    Management.get_report()
+                    sleep(1)
+                elif management_choice == 3:
+                    Client.clients_list()
+                    sleep(1)
+                elif management_choice == 4:
+                    break
+                else:
+                    print("\033[31mError, please try again.\033[m")
+                    print()
+                    print(" " * 15, " . " * 10, " " * 10)
+                    continue
+        elif choice == 3:
+            fin = Start("""\033[33m
+    ╔╗ ╦ ╦╔═╗┬┬┬  ╔═╗╔═╗╔╦╗╔═╗  ╔╗ ╔═╗╔═╗╦╔═  ╔═╗╔═╗╔═╗╔╗╔
+    ╠╩╗╚╦╝║╣ │││  ║  ║ ║║║║║╣   ╠╩╗╠═╣║  ╠╩╗  ╚═╗║ ║║ ║║║║
+    ╚═╝ ╩ ╚═╝ooo  ╚═╝╚═╝╩ ╩╚═╝  ╚═╝╩ ╩╚═╝╩ ╩  ╚═╝╚═╝╚═╝╝╚╝
+""")
+            print(fin.get_frase())
+            sleep(0.3)
+            print("\033[32m", u"\U0001F697 ", end='')
+            sleep(0.5)
+            print(u"\U0001F697 ", end='')
+            sleep(0.5)
+            print(u"\U0001F697 ", end='')
+            sleep(0.5)
+            print()
+            break
+        else:
+            print("\033[31mPlease try again...\033[m")
+            print()
+            print(" " * 15, " . " * 10, " " * 10)
+    except ValueError:
+        print("\033[31mPlease enter a valid number...\033[m")
+        print()
+        print(" " * 15, " . " * 10, " " * 10)
